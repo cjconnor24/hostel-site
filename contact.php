7dced3b3db02399bd7cba50664a9a25483e154da
Chris Connor <cjconnor24@connordesign.com>
2017-10-09 10:46:44 +0100

    Adding php for debugging form responses

--- contact.php
+++ contact.php
@@ -1,7 +1,75 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: chrisconnor
- * Date: 09/10/2017
- * Time: 09:40
- */
+include('includes/header.php');
+?>
+
+<section id="content">
+
+    <div class="wrapper">
+
+        <div class="row">
+
+            <div class="col-6">
+                <h1>Contact Us</h1>
+
+                <p>To get in contact with us, simply fill in the form below</p>
+
+                <div class="contact-form">
+
+                    <?php
+                    if(isset($_POST['submit'])){
+
+                        ?>
+                    <div class="display success">
+
+                        <h2>Thank You</h2>
+                        <p>Thanks for getting in contact with us, someone will be in touch with you shortly.</p>
+
+<pre>
+TEMP // DEBUG
+<?php echo print_r($_POST);?>
+</pre>
+
+                    </div>
+                    <?php } ?>
+
+                    <form action="" method="post">
+
+                        <div class="field">
+                        <label for="name">Your Name</label>
+                        <input type="text" name="full_name" value="" placeholder="Your Name" required>
+                        </div>
+
+                        <div class="field">
+                        <label for="email">Email Address</label>
+                        <input type="email" name="email" value="" placeholder="Your Email Address" required>
+                        </div>
+                        
+                        <div class="field">
+                            <label for="message">Message</label>
+                            <textarea name="message" id="message" cols="30" rows="10" required></textarea>
+                        </div>
+
+                        <div class="field">
+                            <button type="submit" name="submit"><i class="fa fa-envelope"></i> Send Message</button>
+                            <button type="reset">Clear Form</button>
+                        </div>
+
+                    </form>
+
+                </div>
+
+            </div>
+
+            <div class="col-6">
+                <h1>Find Us</h1>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+<?php
+include('includes/footer.php');
+?>
