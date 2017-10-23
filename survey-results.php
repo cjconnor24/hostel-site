e0146ef645c447b5e2b4fb3cbd5a2265d319c636
Chris Connor <cjconnor24@connordesign.com>
2017-10-23 09:09:57 +0100

    Splitting map logic into file

--- survey-results.php
+++ survey-results.php
@@ -10,130 +10,17 @@
 <!--    </section>-->
 <section id="location-map"></section>
 
-<script>
-    function myMap() {
-
-        var locheil = {lat: 56.8493272, lng: -5.2062179};
-
-        var mapProp= {
-            center:locheil,
-            zoom:9,
-            disableDefaultUI: true
-        };
-
-        var styles = {
-            default: null,
-            silver: [
-                {
-                    elementType: 'geometry',
-                    stylers: [{color: '#f5f5f5'}]
-                },
-                {
-                    elementType: 'labels.icon',
-                    stylers: [{visibility: 'off'}]
-                },
-                {
-                    elementType: 'labels.text.fill',
-                    stylers: [{color: '#616161'}]
-                },
-                {
-                    elementType: 'labels.text.stroke',
-                    stylers: [{color: '#f5f5f5'}]
-                },
-                {
-                    featureType: 'administrative.land_parcel',
-                    elementType: 'labels.text.fill',
-                    stylers: [{color: '#bdbdbd'}]
-                },
-                {
-                    featureType: 'poi',
-                    elementType: 'geometry',
-                    stylers: [{color: '#eeeeee'}]
-                },
-                {
-                    featureType: 'poi',
-                    elementType: 'labels.text.fill',
-                    stylers: [{color: '#757575'}]
-                },
-                {
-                    featureType: 'poi.park',
-                    elementType: 'geometry',
-                    stylers: [{color: '#e5e5e5'}]
-                },
-                {
-                    featureType: 'poi.park',
-                    elementType: 'labels.text.fill',
-                    stylers: [{color: '#9e9e9e'}]
-                },
-                {
-                    featureType: 'road',
-                    elementType: 'geometry',
-                    stylers: [{color: '#ffffff'}]
-                },
-                {
-                    featureType: 'road.arterial',
-                    elementType: 'labels.text.fill',
-                    stylers: [{color: '#757575'}]
-                },
-                {
-                    featureType: 'road.highway',
-                    elementType: 'geometry',
-                    stylers: [{color: '#dadada'}]
-                },
-                {
-                    featureType: 'road.highway',
-                    elementType: 'labels.text.fill',
-                    stylers: [{color: '#616161'}]
-                },
-                {
-                    featureType: 'road.local',
-                    elementType: 'labels.text.fill',
-                    stylers: [{color: '#9e9e9e'}]
-                },
-                {
-                    featureType: 'transit.line',
-                    elementType: 'geometry',
-                    stylers: [{color: '#e5e5e5'}]
-                },
-                {
-                    featureType: 'transit.station',
-                    elementType: 'geometry',
-                    stylers: [{color: '#eeeeee'}]
-                },
-                {
-                    featureType: 'water',
-                    elementType: 'geometry',
-                    stylers: [{color: '#c9c9c9'}]
-                },
-//                {
-//                    featureType: 'water',
-//                    elementType: 'labels.text.fill',
-//                    stylers: [{color: '#9e9e9e'}]
-//                }
-            ]};
-
-
-
-
-
-        var map=new google.maps.Map(document.getElementById("location-map"),mapProp);
-        map.setOptions({styles: styles['silver']});
-
-        var marker = new google.maps.Marker({
-            position:locheil,
-            map:map,
-            icon:'http://www.hostel.dev/images/map-marker.png'
-        })
-
-
-    }
-</script>
+<script src="/js/google-map.js"></script>
 
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAbs8Wb7fnp6cMbiuuWfJbX-3X3ItHC2Rw&callback=myMap"></script>
 
     <section id="content">
 
         <div class="wrapper">
+
+            <div class="row">
+
+                <div class="col-6">
 
             <h1>Travel Survey Results</h1>
 
@@ -215,10 +102,50 @@
                 }
             </script>
 
+                </div>
+
+                <div class="col-6">
+
+            <h2>Assistance List</h2>
+            <p>Below are the list of respondants who require assistance</p>
+
+            <?php
+            $query = $conn->prepare("SELECT first, last, email from survey WHERE assistance = 1;");
+            $query->execute();
+            $results = $query->fetchAll(PDO::FETCH_ASSOC);
+            ?>
+            <table class="assistance-table">
+                <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email Address</th>
+                </tr>
+                </thead>
+
+            <tbody>
+            <?php
+            foreach($results as $rows){
+                ?>
+
+                <tr>
+                    <td><?php echo $rows['first'];?></td>
+                    <td><?php echo $rows['last'];?></td>
+                    <td><?php echo $rows['email'];?></td>
+                </tr>
+
+            <?php
+            }
+            ?>
+            </tbody>
+            </table>
+
+                </div>
             </div>
 
-
         </div>
+
+            </div>
 
     </section>
 
