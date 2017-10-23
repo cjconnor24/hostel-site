71bb4f1d8175395c6fa652f6f0ef4f38ce7a86bb
Chris Connor <cjconnor24@connordesign.com>
2017-10-23 09:28:40 +0100

    Styled the assistance table and added a download link

--- survey-results.php
+++ survey-results.php
@@ -8,10 +8,10 @@
 <!--        <p><a href="#" class="btn">Find out more</a></p>-->
 <!--        </div>-->
 <!--    </section>-->
+
+<!--LOCATION MAP-->
 <section id="location-map"></section>
-
 <script src="/js/google-map.js"></script>
-
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAbs8Wb7fnp6cMbiuuWfJbX-3X3ItHC2Rw&callback=myMap"></script>
 
     <section id="content">
@@ -42,65 +42,11 @@
             </table>
 
             <script type="text/javascript">
-                var element;
+
                 var data = {"survey": <?php echo $results;?> };
 
-                var total = 0;
-                var count = data.survey;
-
-                // CALCULATE THE TOTAL ENTRIES
-                for(i=0;i<count.length;i++){
-                    total += parseInt(count[i].total);
-                }
-
-                console.log(total);
-
-                for (element in data.survey) {
-                    var names = data.survey[element].mode;
-                    var values = data.survey[element].total;
-
-//                    ROW
-                    var node = document.createElement("tr");
-
-//                    CELL
-                    var subnode1 = document.createElement("td");
-//                    var textnode1 = document.createTextNode('fa-'+names.toLowerCase());
-                    var textnode1 = document.createElement("span");
-                    textnode1.setAttribute('class','fa fa-'+names.toLowerCase());
-
-
-//                    CELL
-                    var subnode3 = document.createElement("td");
-                    var subsubnode = document.createElement("canvas");
-
-                    subnode1.appendChild(textnode1);
-                    subnode3.appendChild(subsubnode);
-
-                    var att = subsubnode.setAttribute("id", names);
-
-
-                    node.appendChild(subnode1);
-                    node.appendChild(subnode3);
-                    document.getElementById("chart").appendChild(node);
-
-                    var c = document.getElementById(names);
-                    var bar = c.getContext("2d");
-                    var length = values;
-                    if (values > 0) {
-                        if (length > 100)
-                            length = 100;
-                        bar.fillStyle = "#323232"; //"blue";
-                    }
-                    bar.rect(0, 0, length*100, 100);
-                    bar.fill();
-
-                    bar.fillStyle = "#FFFFFF";
-                    bar.font = "30px Arial";
-                    bar.fillText(values,10,50);
-
-
-                }
             </script>
+                    <script src="/js/survey-chart.js"></script>
 
                 </div>
 
@@ -139,6 +85,8 @@
             ?>
             </tbody>
             </table>
+
+            <p><a href="javascript:alert('doesnot do anything');" class="survey-export"><span class="fa fa-download"></span>Export List</a></p>
 
                 </div>
             </div>
