93b0c9c257825f407c22f4a2e82a3f133bd07531
Chris Connor <cjconnor24@connordesign.com>
2017-11-05 13:27:10 +0000

    adding some styles for lightbox

---
+++ lightbox.php
@@ -0,0 +1,125 @@
+
+<?php include('includes/header.php');?>
+
+    <section class="hero view">
+        <div class="background">
+        <h1>WELCOME to the Backpacker Hostel</h1>
+        <p>This is a sub-heading for more hero information</p>
+        <p><a href="prices.php" class="btn">Find out more</a></p>
+        </div>
+    </section>
+
+    <section id="content">
+
+        <div class="wrapper">
+
+            <!--<div class="row">-->
+
+                <!--<div class="col-4"><h2>TEST</h2>-->
+                <!--<p>Here is some body text</p></div>-->
+
+                <!--<div class="col-8"><h2>COLUMN TWO</h2>-->
+                <!--<p>THIS IS ANOTHER COLOUMN</p>-->
+                <!--</div>-->
+
+            <!--</div>-->
+
+
+            <div class="clear">
+            <h1>Welcome to BACKPACKER Hostels</h1>
+
+
+            <p><img src="/images/hostel/outside.jpg" alt="Hostel Exterior" class="right shadow" ></p>
+
+            <p>The Hostel has one of the best locations in Scotland. From the attractive loch-side garden, the views across to mountain range are breathtaking. Good facilities mean the hostel is popular with walkers and tourist alike.</p>
+            <p>There are otters, badgers and pine martins in the area. Red deer are numerous, and on the mountains ravens, ptarmigan, golden eagles and mountain hares can be seen on occasion</p>
+            <p>... well situated for walking and exploring some of Scotland's most celebrated scenery"!</p>
+            <p>With no mobile phone reception the emphasis here is on tranquillity and relaxation! </p>
+
+            </div>
+
+            <h2>Room Rates</h2>
+
+            <p>Below are our current room rates for this season.</p>
+            <div class="prices">
+
+
+                <div class="price-col">
+
+                    <ul>
+                        <li class="header">Dormitory</li>
+                        <li class="price">£9.50 per night</li>
+                        <li>Perfect for Groups</li>
+                        <li>Test Line</li>
+                        <li>Another Feature</li>
+                        <li class="footer"><a href="#">More Info</a></li>
+                    </ul>
+
+                </div>
+
+                <div class="price-col">
+
+                    <ul>
+                        <li class="header highlight">Group Room</li>
+                        <li class="price">£11 per night</li>
+                        <li>Perfect for Groups</li>
+                        <li>Test Line</li>
+                        <li>Another Feature</li>
+                        <li class="footer"><a href="#">More Info</a></li>
+                    </ul>
+
+                </div>
+
+                <div class="price-col">
+
+                    <ul>
+                        <li class="header">Double Room</li>
+                        <li class="price">£25 per night</li>
+                        <li>Perfect for Groups</li>
+                        <li>Test Line</li>
+                        <li>Another Feature</li>
+                        <li class="footer"><a href="#">More Info</a></li>
+                    </ul>
+
+                </div>
+
+            </div>
+
+            <h2>Gallery Example</h2>
+            <p>Below is a text image gallery for testing.</p>
+            <?php include('gallery.php'); ?>
+
+            <h1>HTML Ipsum Presents</h1>
+
+            <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+
+            <h2>Header Level 2</h2>
+
+            <ol>
+                <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+                <li>Aliquam tincidunt mauris eu risus.</li>
+            </ol>
+
+            <blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>
+
+            <h3>Header Level 3</h3>
+
+            <ul>
+                <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+                <li>Aliquam tincidunt mauris eu risus.</li>
+            </ul>
+
+            <pre><code>
+#header h1 a {
+display: block;
+width: 300px;
+height: 80px;
+}
+</code></pre>
+
+        </div>
+
+    </section>
+
+<?php include('includes/footer.php');?>
+
