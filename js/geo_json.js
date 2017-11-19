cc74d74dda3c38ec10ccf2b992431ffbd3cd96a8
Chris Connor <cjconnor24@connordesign.com>
2017-11-19 19:02:58 +0000

    Adding GeoJSON file

---
+++ js/geo_json.js
@@ -0,0 +1,102 @@
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#0b7e00"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -4.549627304077148,
+          56.08942157634129
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#0b7e00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -4.549541473388672,
+            56.08942157634129
+          ],
+          [
+            -4.555034637451172,
+            56.088655411938795
+          ],
+          [
+            -4.563446044921875,
+            56.0875540239099
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#0b7e00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -4.549627304077148,
+            56.08946946111044
+          ],
+          [
+            -4.553918838500977,
+            56.08975676847506
+          ],
+          [
+            -4.563617706298828,
+            56.08884695446819
+          ],
+          [
+            -4.56507682800293,
+            56.089230036669434
+          ],
+          [
+            -4.589109420776367,
+            56.08659626958982
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#0b7e00",
+        "marker-size": "medium",
+        "marker-symbol": "triangle"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -4.563531875610351,
+          56.10158239499946
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ff0005",
+        "marker-size": "medium",
+        "marker-symbol": "bank"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -4.539906978607178,
+          56.08223818675173
+        ]
+      }
+    }
+  ]
+}
